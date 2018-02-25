2c4443069417b96fe9731db55b8ad9ee5f306299
Audrius Petrekis <audriusp@wix.com>
2018-02-25 19:17:32 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace drawRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 0;
+            int heigth = 0;
+
+            Console.WriteLine("Ener rectangle width: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ener rectangle heigth: ");
+            heigth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Width: " + width + " Heigth: " + heigth);
+
+            for (int line = 1; line < heigth + 1; line++) {
+                for (int column = 1; column < width + 1; column++) {
+                    if(line == heigth || line == 1 || column == 1 || column == width) {
+                        Console.Write("#");
+                    } else {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
